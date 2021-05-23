707a2b9687296cf12f24df95a22bc3a6321b532a
piyush <varshneypiyush67@gmail.com>
2021-05-23 11:09:08 +0530

    initial

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const PORT=process.env.PORT || 3000;
+
+app.use(express.static('public'));  //for css
+app.use(express.json());            //for json
+
+const connectDB = require('./config/db');
+connectDB();
+
+
+const corsOptions={
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions));
+//Template engine
+app.set('views',path.join(__dirname,'/views')); //for ejs
+app.set('view engine','ejs');
+
+
+//Routes
+app.use('/api/files', require('./routes/files'));
+app.use('/files',require('./routes/show'));  //for render to dowload page
+app.use('/files/download',require('./routes/download'));
+
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}`) ;
+})
